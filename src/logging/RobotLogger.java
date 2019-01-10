c44bf5a857b11c6891d8285d37381a3d626a918f
Rishi Nath <rishi.nath@gmail.com>
2019-01-09 20:37:25 -0800

    Added basic formatting

--- src/logging/RobotLogger.java
+++ src/logging/RobotLogger.java
@@ -5,16 +5,23 @@ import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import logging.LogFormatter;
+
 public class RobotLogger {
     static private FileHandler usb;
+    static private LogFormatter formatter;
 
     static public void setup() throws IOException {
         Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-        logger.setLevel(Level.FINEST);
-
         usb = new FileHandler("usb.txt"); // on usb drive
 
+        formatter = new LogFormatter();
+        usb.setFormatter(formatter);
+
         logger.addHandler(usb);
+
+        logger.setLevel(Level.FINEST);
+
     }
 }
